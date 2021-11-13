4310814401c4667de148b701327dae51a8576c17
John Michael So <John.So@maximintegrated.com>
2021-11-13 11:43:12 +0800

    new

--- PROJECT/EDIT_TESTERS.cs
+++ PROJECT/EDIT_TESTERS.cs
@@ -155,7 +155,7 @@ namespace PROJECT
                             else
                             {
                                 MessageBox.Show("INVALID TESTER NAME");
-                                return;
+                                ADD.Clear();
                             }
                         }
                         else
